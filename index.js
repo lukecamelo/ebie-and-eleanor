754e897220f7ac806c1484189e489ba2ce97b00a
luke <azathothbeats@gmail.com>
2018-05-10 18:29:45 -0400

    Different animation for mobile

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 window.sr = ScrollReveal()
 
+const aboutPic = document.querySelector('.about-left')
+const aboutBlurb = document.querySelector('.about-right')
+
+const prod1 = document.querySelector('.p1')
+const prod2 = document.querySelector('.p2')
+const prod3 = document.querySelector('.p3')
+
+if (window.matchMedia("(max-width: 950px)").matches) {
+  aboutPic.classList.remove('about')
+  aboutBlurb.classList.remove('about')
+
+  aboutPic.classList.add('a1')
+  aboutBlurb.classList.add('a2')
+
+  prod1.classList.remove('p-anim')
+  prod2.classList.remove('p-anim')
+  prod3.classList.remove('p-anim')
+  
+  prod1.classList.add('p-anim1')
+  prod2.classList.add('p-anim2')
+  prod3.classList.add('p-anim3')
+
+  
+}
+
 sr.reveal('.about', {origin: 'left', rotate: {x: 2, y: 2, z: 2}}, 50)
-sr.reveal('.product', 50)
+sr.reveal('.a1', {origin: 'left'})
+sr.reveal('.a2', {origin: 'right'})
+
+sr.reveal('.p-anim', 50)
+sr.reveal('.p-anim1', {origin: 'bottom'})
+sr.reveal('.p-anim2', {origin: 'bottom'})
+sr.reveal('.p-anim3', {origin: 'bottom'})
